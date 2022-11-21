92961d0bf8761504bb4d0b9ca2d6e6c59c9e4cc0
naegonggae <https://github.com/naegonggae/>
2022-11-21 11:22:16 +0900

    Springboot Mustache bbs project 빌드 1. application.yml 추가

---
+++ src/main/java/com/mustache/bbsproject/MustacheBbsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mustache.bbsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustacheBbsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MustacheBbsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustache/bbsproject/MustacheBbsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.bbsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustacheBbsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
